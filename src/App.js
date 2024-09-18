649c0d20ad8a60a43d3b85ea83f7a506cec40bc9
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2024-09-18 17:36:46 +0545

    starting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,10 +5,13 @@ function App() {
   return (
   <div className="main">
      <h1> Learning React</h1>
-     <div>hello world!</div>
-     <br/> 
-     
-     {variable && <p>true</p>}
+     <h2>
+      What is React?
+     </h2>
+     <p>ReactJS, also known as React, is a popular JavaScript library for building user interfaces. It is also referred to as a front-end JavaScript library. It was developed by Facebook and is widely used for creating dynamic and interactive web applications. In this article, we’ll explore the key concepts of React.</p>
+     <p>Libraries provide reusable code components that can be integrated into existing applications.</p>
+     <h2>What is Next.js?</h2>
+    <p>Next.js is a React framework that enables several extra features, including server-side rendering and generating static website.Next.Js can be thought of as an opinionated way to write react by simplifying many best practices that would have taken more effort by plain react (create-react-app).</p>
     
   </div>
   );
